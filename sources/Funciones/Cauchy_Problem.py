ba9c2d560031596e21a49e635e9c9b7d3743bdac
DanielCeronGranda <113105978+DanielCeronGranda@users.noreply.github.com>
2022-12-07 10:31:02 +0100

    Update Cauchy_Problem.py

--- sources/Funciones/Cauchy_Problem.py
+++ sources/Funciones/Cauchy_Problem.py
@@ -27,17 +27,3 @@ def Cauchy_problem_Leap_Frog(Temporal_scheme, U0, n, deltat, F, U1, d): #d = dim
         U1 = U
         continue
     return (matrizU)
-
-#Función del problema de Cauchy adaptada al problema de N cuerpos
-def Cauchy_problem_N_body(Temporal_scheme, U0, n, deltat, F, F0, d, Nb, Nc): #d = dimensión de U
-    t=0
-    c = int(d)
-    matrizU = array(zeros([n,c]))
-    for i in range (n):
-        t = t+deltat
-        U = Temporal_scheme(U0, deltat, t, F0, F)
-        matrizU[i,:] = U 
-        U0 = U
-        F0 = F(U, t, Nb, Nc) #Obtengo mi nuevo valor de F(U,t)
-        continue
-    return (matrizU)
